dfbd7ad8800379cfda2b3e52f7df4bcd04741db5
Vaibhav Jha <vaibhavjha@microsoft.com>
2024-02-16 16:41:41 +0530

    Removed user defined variables

--- Samples/spe-ocr-sample/src/common/constants.ts
+++ Samples/spe-ocr-sample/src/common/constants.ts
@@ -1,7 +1,7 @@
-export const CLIENT_ENTRA_APP_CLIENT_ID = 'da5e4cc8-42b7-420d-9f54-08df6bd62599';
+export const CLIENT_ENTRA_APP_CLIENT_ID = '';
 export const CLIENT_ENTRA_APP_AUTHORITY = 'https://login.microsoftonline.com/organizations/';
 export const CLIENT_SERVER_URL = 'http://localhost:3000';
 
 export const API_SERVER_URL = 'http://localhost:3001';
 
-export const CONTAINER_TYPE_ID = 'f5ccb3d0-3de0-0dc9-08da-045a08c7e278';
+export const CONTAINER_TYPE_ID = '';
