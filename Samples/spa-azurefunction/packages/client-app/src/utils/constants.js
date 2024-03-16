d7df082ae7abf20b2ba804b56c6737a6461c4be0
Alex Jordache <therealAJ@users.noreply.github.com>
2024-03-15 22:43:06 -0700

    Update constants.js

--- Samples/spa-azurefunction/packages/client-app/src/utils/constants.js
+++ Samples/spa-azurefunction/packages/client-app/src/utils/constants.js
@@ -1,2 +1,4 @@
 export const GRAPH_SCOPE_USER_READ = 'User.Read';
 export const GRAPH_SCOPE_FILES_READ_WRITE_ALL = 'Files.ReadWrite.All';
+export const localFunctionHost = 'http://localhost:7071';
+export const remoteFunctionHost = '';
