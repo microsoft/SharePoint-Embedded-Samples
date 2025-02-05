9da22ccf4e93d56c2f51f85e07684676873f7253
Marc Windle <108372230+marcwindle@users.noreply.github.com>
2025-02-05 17:59:50 +0000

    Set API URL in environment

--- .devcontainer/spe-typescript-react-azurefunction/postStart.sh
+++ .devcontainer/spe-typescript-react-azurefunction/postStart.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Make React client and Azure function API ports public
 gh codespace ports visibility 8080:public 7072:public -c $CODESPACE_NAME
 
-export REACT_APP_SAMPLE_API_URL="$(gh codespace ports -c $CODESPACE_NAME --json sourcePort,browseUrl --jq '.[] | select(.sourcePort==7072).browseUrl')"
-
+# Set the API URL in the environment if needed
+API_URL="$(gh codespace ports -c $CODESPACE_NAME --json sourcePort,browseUrl --jq '.[] | select(.sourcePort==7072).browseUrl')"
+API_URL+="/api"
+if [$REACT_APP_SAMPLE_API_URL != API_URL]; then
+    SET_VAR_CMD="export REACT_APP_SAMPLE_API_URL=${API_URL}"
+    echo $SET_VAR_CMD >> ~/.bashrc
+fi
