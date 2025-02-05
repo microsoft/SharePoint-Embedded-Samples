11dd49e760117fe285c55eb556074942475ee293
Marc Windle <108372230+marcwindle@users.noreply.github.com>
2025-02-05 18:27:20 +0000

    Fix API URL env setup

--- .devcontainer/spe-typescript-react-azurefunction/postStart.sh
+++ .devcontainer/spe-typescript-react-azurefunction/postStart.sh
@@ -6,7 +6,10 @@ gh codespace ports visibility 8080:public 7072:public -c $CODESPACE_NAME
 # Set the API URL in the environment if needed
 API_URL="$(gh codespace ports -c $CODESPACE_NAME --json sourcePort,browseUrl --jq '.[] | select(.sourcePort==7072).browseUrl')"
 API_URL+="/api"
-if [$REACT_APP_SAMPLE_API_URL != API_URL]; then
+
+if [ "$REACT_APP_SAMPLE_API_URL" != "$API_URL" ]; then
     SET_VAR_CMD="export REACT_APP_SAMPLE_API_URL=${API_URL}"
     echo $SET_VAR_CMD >> ~/.bashrc
 fi
+
+source ~/.bashrc
